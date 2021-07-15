99f3f4784c2f00bf7339492f269699e5db182091
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-15 21:30:49 +0500

    fix: правка api. добавил cors политику

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -120,9 +120,13 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' });
-      return res.send({ token });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+
+      res.send({ token });
     })
     .catch((err) => {
       throw new NotAuthError(err.message);
