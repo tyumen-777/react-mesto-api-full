8eaa6222b13151c3a258ea899f8ac2af08217f43
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-18 18:17:23 +0500

    first commit

---
+++ backend/helpers/isAuthorized.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const isAuthorized = (token) => {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return false;
+  }
+};
+module.exports = isAuthorized;
