380f81b8aa1b0c1a7361d0d553fc7a86c9c72d22
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-18 23:13:05 +0500

    first commit

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -31,7 +31,7 @@ function Main(props) {
             </section>
             <section className="elements">
                 {cards.map((card) =>
-                  (<Card key={card.card._id} card={card} onCardClick={onCardClick}
+                  (<Card key={card._id} card={card} onCardClick={onCardClick}
                                           onCardLike={onCardLike} onCardDelete={onCardDelete}/>))}
             </section>
         </main>
