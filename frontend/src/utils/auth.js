a5f25b5067109ff6d45b0460b1870d313398a426
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-20 21:04:38 +0500

    first commit

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -44,7 +44,7 @@ export const getContent = (token) => {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `${token}`,
+            'Authorization': `Bearer ${token}`,
         },
     })
         .then(checkResponse)
