3b2d521e513f1485f9a49eefbd75d13c2b9a9a6c
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-21 12:25:54 +0500

    first commit

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -39,12 +39,12 @@ export const authorize = ({email, password}) => {
         })
 };
 
-export const getContent = () => {
+export const getContent = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `${localStorage.getItem('jwt')}`,
+            'Authorization': `Bearer ${token}`,
         },
     })
         .then(checkResponse)
