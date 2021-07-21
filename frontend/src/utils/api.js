8c58c8d36eda7f4bb470ddbff2ed65bd2428ade4
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-21 20:10:32 +0500

    first commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -88,7 +88,7 @@ class Api {
     updateHeaders() {
       this._headers = {
         'Content-Type': 'application/json',
-        'Authorization': `${localStorage.getItem('jwt')}`
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
       }
     }
 }
@@ -97,7 +97,7 @@ class Api {
 const api = new Api({
     address: 'https://api.tyumen-777.nomoredomains.monster',
     headers: {
-        'Authorization': `${localStorage.getItem('jwt')}`,
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
         'Content-type': 'application/json',
     }
 })
