00f863cba2b4b544331fba480c17d8270344a993
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-21 19:45:53 +0500

    first commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,12 +4,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  const { authorization } = req.headers;
 
-  if (!token) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(401)
       .send({ message: 'Необходима авторизация' });
   }
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
