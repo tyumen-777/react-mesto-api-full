6e803c1b6992ce54061acd81f28c4f3ab8a24131
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-21 19:55:02 +0500

    first commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const NotAuthError = require('../errors/not-auth-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -7,8 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new NotAuthError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
 
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new NotAuthError('Необходима авторизация');
   }
   req.user = payload;
 
