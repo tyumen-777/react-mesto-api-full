24f6a17eaac9e0df0bc8ea6ee8f0598a3af9d3e9
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-21 18:49:17 +0500

    first commit

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -40,6 +40,7 @@ export const authorize = ({email, password}) => {
 };
 
 export const getContent = (token) => {
+  console.log(token)
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
