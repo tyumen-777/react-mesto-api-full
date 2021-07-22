19faf0eb1c6850096d030563b49405dcdc9fadb8
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-07-22 18:15:33 +0500

    first commit

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,7 +5,7 @@ const NotAuthError = require('../errors/not-auth-error');
 const NotFoundError = require('../errors/not-found-error');
 const BadRequestError = require('../errors/bad-request-error');
 const ConflictError = require('../errors/conflict-error');
-const isAuthorized = require('../helpers/isAuthorized');
+// const isAuthorized = require('../helpers/isAuthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -18,12 +18,13 @@ const getUsers = (req, res, next) => {
 };
 
 const getMe = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (!isAuthorized(token)) {
-    throw new BadRequestError('Доступ запрещен');
-  }
-  return User.findById(req.user._id)
+  // const token = req.headers.authorization;
+  //
+  // if (!isAuthorized(token)) {
+  //   throw new BadRequestError('Доступ запрещен');
+  // }
+  // return
+  User.findById(req.user._id)
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Нет пользователя с таким id');
